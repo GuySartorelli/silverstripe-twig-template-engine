126567bdbd42f50619c0b9371817425b60d17565
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2022-07-06 17:58:09 +1200

    FIX conform with new interface signature.

--- src/TemplateEngine.php
+++ src/TemplateEngine.php
@@ -3,12 +3,19 @@
 namespace SilverStripe\Twig;
 
 use BadMethodCallException;
+use SilverStripe\Template\View\SSViewer_Scope;
 use SilverStripe\View\TemplateEngine as ViewTemplateEngine;
+use SilverStripe\View\ViewableData;
 
 class TemplateEngine implements ViewTemplateEngine
 {
-    public function renderString(string $content, bool $includeDebugComments = false, string $templateName = ''): string
+    public function process(string $template, ViewableData $item, array $overlay, array $underlay, ?SSViewer_Scope $inheritedScope = null): string
     {
         throw new BadMethodCallException('This has not been implemented yet.');
+    }
+
+    public static function flush()
+    {
+        // no-op for now
     }
 }
